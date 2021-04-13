e701562f0ae398b1c26786bc5a3788bfde491cb8
ruoyankong <ruoyankong@gmail.com>
2021-04-13 14:17:41 -0500

    fileupload

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+//import com.example.demo.storage.StorageService;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import com.example.demo.storage.StorageService;
 
+//@EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
 @RestController
 public class DemoApplication {
@@ -45,10 +50,20 @@ public class DemoApplication {
 	@Bean
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
-			Quote quote = restTemplate.getForObject(
-					"https://quoters.apps.pcfone.io/api/random", Quote.class);
-			log.info(quote.toString());
+			while (true) {
+				Quote quote = restTemplate.getForObject(
+						"https://quoters.apps.pcfone.io/api/random", Quote.class);
+				log.info(quote.toString());
+			}
 		};
 	}
+
+//	@Bean
+//	CommandLineRunner init(StorageService storageService) {
+//		return (args) -> {
+//			storageService.deleteAll();
+//			storageService.init();
+//		};
+//	}
 
 }
